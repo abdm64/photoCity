8565bfe6bb20262529327b40abb43e5b8d41f419
Messlleka Abdellah <39844646+abdm64@users.noreply.github.com>
2018-10-11 13:59:08 +0100

    add source code

--- pixel-city/Controller/PopVC.swift
+++ pixel-city/Controller/PopVC.swift
@@ -6,50 +6,50 @@
 //  Copyright © 2018 ABD. All rights reserved.
 //
 
-import UIKit
-
-class PopVC: UIViewController, UIGestureRecognizerDelegate {
-
-    @IBOutlet weak var popImageView: UIImageView!
-    
-    @IBOutlet weak var titleLbl: UILabel!
-    
-    
-    var passedImage: UIImage!
-    var titlePassed : String!
-    
-    func initData(forImage image: UIImage,forText text : String) {
-        self.passedImage = image
-        self.titlePassed = text
-        
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        popImageView.image = passedImage
-        titleLbl.text = titlePassed
-        addDoubleTap()
-        swipeDown()
-    }
-    
-    func addDoubleTap() {
-        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(screenWasDoubleTapped))
-        doubleTap.numberOfTapsRequired = 2
-        doubleTap.delegate = self
-        view.addGestureRecognizer(doubleTap)
-    }
-    func swipeDown(){
-        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(screenWasDoubleTapped))
-        swipeDown.direction = .down
-        swipeDown.delegate = self
-        view.addGestureRecognizer(swipeDown)
-        
-    }
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .default
-    }
-    @objc func screenWasDoubleTapped() {
-        dismiss(animated: true, completion: nil)
-    }
-
-}
+//import UIKit
+//
+//class PopVC: UIViewController, UIGestureRecognizerDelegate {
+//
+//    @IBOutlet weak var popImageView: UIImageView!
+//
+//    @IBOutlet weak var titleLbl: UILabel!
+//
+//
+//    var passedImage: UIImage!
+//    var titlePassed : String!
+//
+//    func initData(forImage image: UIImage,forText text : String) {
+//        self.passedImage = image
+//        self.titlePassed = text
+//
+//    }
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        popImageView.image = passedImage
+//        titleLbl.text = titlePassed
+//        addDoubleTap()
+//        swipeDown()
+//    }
+//
+//    func addDoubleTap() {
+//        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(screenWasDoubleTapped))
+//        doubleTap.numberOfTapsRequired = 2
+//        doubleTap.delegate = self
+//        view.addGestureRecognizer(doubleTap)
+//    }
+//    func swipeDown(){
+//        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(screenWasDoubleTapped))
+//        swipeDown.direction = .down
+//        swipeDown.delegate = self
+//        view.addGestureRecognizer(swipeDown)
+//
+//    }
+//    override var preferredStatusBarStyle: UIStatusBarStyle {
+//        return .default
+//    }
+//    @objc func screenWasDoubleTapped() {
+//        dismiss(animated: true, completion: nil)
+//    }
+//
+//}
